d9115b1b7031cfb355200a452185261df0233e15
rizwan-0987 <leocoin4737@gmail.com>
2025-11-05 10:43:26 +0500

    backend res error resolved

--- Bg-removal-backend/controllers/UserController.js
+++ Bg-removal-backend/controllers/UserController.js
@@ -52,9 +52,23 @@ const clerkWebhooks = async (req, res) => {
         break;
     }
   } catch (error) {
-    console.log(error.message);
-    res.json({ success: false, message:error.message });
+      console.log(error.message);
+      res.json({ success: false, message: error.message });
   }
 };
 
-export{clerkWebhooks}
+
+//API COntroller function to get user available credits data
+const userCredits = async (req, res) => {
+    try {
+        const { clerkId } = req.body
+        const userData = await userModel.findOne({ clerkId })
+        res.json({success:true,credits:userData.creditBalance})
+    } catch (error) {
+        console.log(error.message);
+        res.json({ success: false, message: error.message }); 
+    }
+}
+
+
+export { clerkWebhooks,userCredits }
